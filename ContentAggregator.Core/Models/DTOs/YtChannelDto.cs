779cd5f78e01e2358d750ac1c6f7a9ad6f0d56d4
shota.nx <shota.nx@gmail.com>
2025-04-06 14:12:15 +0400

    Changed ChannelTitle to nullable in YtChannelDto

--- ContentAggregator.Core/Models/DTOs/YtChannelDto.cs
+++ ContentAggregator.Core/Models/DTOs/YtChannelDto.cs
@@ -4,7 +4,7 @@
     {
         public required string ChannelSuffix { get; set; }
         public byte ActivityLevel { get; set; }
-        public required string ChannelTitle { get; set; }
+        public string? ChannelTitle { get; set; }
         public string? TitleKeywords { get; set; }
     }
 }
