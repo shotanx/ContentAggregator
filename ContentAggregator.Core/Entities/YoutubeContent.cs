b3677c4db0d7538bc63fd8909002222b3e073f21
shota.nx <shota.nx@gmail.com>
2025-04-20 23:58:18 +0400

    Changed a column name in YoutubeContent table

--- ContentAggregator.Core/Entities/YoutubeContent.cs
+++ ContentAggregator.Core/Entities/YoutubeContent.cs
@@ -11,13 +11,13 @@ namespace ContentAggregator.Core.Entities
         public TimeSpan VideoLength { get; set; }
         public DateTimeOffset VideoPublishedAt { get; set; }
         public bool NotRelevant { get; set; } = false; // Longer than required threshold but not political/social/etc.
-        public bool FbPostedStatus { get; set; } = false;
         public string? SubtitlesEngSRT { get; set; }
         public string? SubtitlesFiltered { get; set; }
         public string? VideoSummaryEng { get; set; }
         public string? VideoSummaryGeo { get; set; }
         public string? AdditionalComments { get; set; }
         public bool FbPosted { get; set; } = false;
+        public bool NeedsRefetch {  get; set; } = false;
 
         public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow; // Now: e.g., 2023-10-05T14:30:00+02:00 ---- UtcNow: e.g., 2023-10-05T12:30:00+00:00
         public DateTimeOffset? UpdatedAt { get; set; }
---
+++ ContentAggregator.Infrastructure/Migrations/20250420195046_AddNeedsRefetchToYoutubeContent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContentAggregator.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNeedsRefetchToYoutubeContent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FbPostedStatus",
+                table: "YoutubeContent",
+                newName: "NeedsRefetch");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NeedsRefetch",
+                table: "YoutubeContent",
+                newName: "FbPostedStatus");
+        }
+    }
+}
